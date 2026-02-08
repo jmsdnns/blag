51b2aa7b49ca9bca82f184ab8f484832f6724d29
Jms Dnns <jdennis@gmail.com>
2026-02-08 15:37:48 -0500

    Fixed overlay font and centering

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,18 @@
 var body = document.querySelector('body');
 var menuTrigger = document.querySelector('#toggle-menu-main-mobile');
 var menuContainer = document.querySelector('#menu-main-mobile');
-var hamburgerIcon = document.querySelector('.hamburger');
+var menuClose = document.querySelector('#close-menu-mobile');
 
 if (menuTrigger !== null) {
   menuTrigger.addEventListener('click', function(e) {
-    menuContainer.classList.toggle('open');
-    hamburgerIcon.classList.toggle('is-active');
-    body.classList.toggle('lock-scroll');
+    menuContainer.classList.add('open');
+    body.classList.add('lock-scroll');
+  });
+}
+
+if (menuClose !== null) {
+  menuClose.addEventListener('click', function(e) {
+    menuContainer.classList.remove('open');
+    body.classList.remove('lock-scroll');
   });
 }
